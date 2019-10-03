8b0444eda3124bbe9fd696956e3dfe6a4a5c26a7
unknown <justin.zhu@sjsu.edu>
2019-10-03 13:32:22 -0700

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This will be the home page.</p>
+        <p>lawrrence sucksss.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
