cdf369040c405b23a1e8366e448bfc549a57bbd6
law-nguyen <law-nguyen>
2019-10-21 15:21:17 -0700

    stuff

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>lul</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
