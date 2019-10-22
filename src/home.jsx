84e58b220a54c0d8873c0a9ad7dbf2af6019ef3c
law-nguyen <law-nguyen>
2019-10-21 17:08:14 -0700

    navbar

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
-
+import "./main.css";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Image,
+  Badge
+} from "react-bootstrap";
 class HomePage extends Component {
   state = {};
   render() {
     return (
       <div>
-        <h1>lul</h1>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">
+            <Image src="./img/icon.jpg" width="50" height="50" />
+          </Navbar.Brand>
+          <Navbar.Brand href="#home">Spartan Forums</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home"></Nav.Link>
+              <Nav.Link href="#new-post">
+                <Image src="./img/new_post.png" width="30" height="30" />
+              </Nav.Link>
+              <Nav.Link href="#inbox">
+                <Image src="./img/mail.jpg" width="30" height="32" />
+                <Badge pill variant="danger" class="lower-right">
+                  2
+                </Badge>
+              </Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
