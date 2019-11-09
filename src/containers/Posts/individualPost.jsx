1b493023d8b89fd847701ba0fad67078db51fb26
law-nguyen <e.lawrence.nguyen@gmail.com>
2019-11-09 13:00:14 -0800

    post header and body

--- src/containers/Posts/individualPost.jsx
+++ src/containers/Posts/individualPost.jsx
@@ -11,7 +11,7 @@ import {
   Col,
   Container
 } from "react-bootstrap";
-
+import "./postsFormat.css";
 class Post extends Component {
   state = {
     //title, body, author, date posted, category, likes, comments
@@ -61,9 +61,34 @@ class Post extends Component {
             </Col>
             <Col md="auto"></Col>
             <Col xl lg="2">
-              <Container class="post">
-                <Row></Row>
-              </Container>
+              <div class="post">
+                <Container>
+                  <Row>
+                    <Col>
+                      <h1>title</h1>
+                      <Row>
+                        <Col>Posted by:</Col>
+                      </Row>
+                      <Row>
+                        <Col>Date Posted:</Col>
+                      </Row>
+                    </Col>
+                    <Col> Category</Col>
+                  </Row>
+                  <Row>
+                    <div class="postBody">
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum."
+                    </div>
+                  </Row>
+                </Container>
+              </div>
             </Col>
           </Row>
         </div>
