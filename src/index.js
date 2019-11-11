5574dac8374050cfe23481df01cbbcbfcf142db4
unknown <justin.zhu@sjsu.edu>
2019-11-10 22:59:12 -0800

    commit3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './utils/registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./utils/registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import HomePage from "./home";
-
+import ProfilePage from "./profile";
 
 //ReactDOM.render(<Routes history={browserHistory} />, document.getElementById('root'));
 //ReactDOM.render(<HomePage/>, document.getElementById('root'));
 
-ReactDOM.render( 
-<div>
-    <HomePage/>
-</div>,
-document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    {/* <HomePage/> */}
+    <ProfilePage />
+  </div>,
+  document.getElementById("root")
+);
 
 registerServiceWorker();
-
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Router, Route } from 'react-router';
+import React from "react";
+import { Router, Route } from "react-router";
 
-import App from './containers/App/database';
-import Posts from './containers/Posts/postPage';
-import AddPost from './containers/AddPost/addPost';
-  
-const Routes = (props) => (
+import App from "./containers/App/database";
+import Posts from "./containers/Posts/postPage";
+import AddPost from "./containers/AddPost/addPost";
+// import ProfilePage from "./profile/ProfilePage";
+
+const Routes = props => (
   <Router {...props}>
-    <Route path="/" component={ App }>
-      <Route path="/posts" component={ Posts } />
-      <Route path="/add-post" component={ AddPost } />
+    <Route path="/" component={App}>
+      <Route path="/posts" component={Posts} />
+      <Route path="/add-post" component={AddPost} />
+      {/* <Route path="/profile" component={ProfilePage} /> */}
     </Route>
   </Router>
 );
